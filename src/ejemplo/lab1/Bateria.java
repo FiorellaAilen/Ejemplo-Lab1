c113b2f0331e2a3ba1ab8acfe1dd7a9b7b93afcd
LuciaYuhasz <luciayuhasz@gmail.com>
2023-08-11 17:54:48 -0300

    create class Bateria

---
+++ src/ejemplo/lab1/Bateria.java
@@ -0,0 +1,32 @@
+
+package ejemplo.lab1;
+
+
+public class Bateria {
+    private String tipoBat;
+    private int carga;
+
+    public Bateria(String tipoBat, int carga) {
+        this.tipoBat = tipoBat;
+        this.carga = carga;
+    }
+
+    public String getTipoBat() {
+        return tipoBat;
+    }
+
+    public void setTipoBat(String tipoBat) {
+        this.tipoBat = tipoBat;
+    }
+
+    public int getCarga() {
+        return carga;
+    }
+
+    public void setCarga(int carga) {
+        this.carga = carga;
+    }
+    
+    
+    
+}
