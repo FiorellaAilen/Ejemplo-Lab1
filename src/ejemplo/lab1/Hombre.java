1f83fc1d33c1ba8b062582c6e561b382f066763b
FiorellaAilen <fiorellaailen@gmail.com>
2023-08-11 18:05:42 -0300

    Modificacion Hombre y Robot

--- src/ejemplo/lab1/Hombre.java
+++ src/ejemplo/lab1/Hombre.java
@@ -3,5 +3,19 @@ package ejemplo.lab1;
 
 public class Hombre {
     
-    private void jugarConRobot(Robot);
+    private Robot jugarConRobot;
+    private String genero;
+    private int edad;
+    private String nombre;
+    private String apellido;
+
+    public Hombre(Robot jugarConRobot, String genero, int edad, String nombre, String apellido) {
+        this.jugarConRobot = jugarConRobot;
+        this.genero = genero;
+        this.edad = edad;
+        this.nombre = nombre;
+        this.apellido = apellido;
+    }
+    
+    
 }
--- src/ejemplo/lab1/Robot.java
+++ src/ejemplo/lab1/Robot.java
@@ -34,4 +34,76 @@ public class Robot {
         this.despierto = despierto;
     }
     
+     public void Avanzar(int cantidadDePasos) {
+        int consumo=(cantidadDePasos/100)*10;
+        if (bateria.getCarga()>=consumo) {
+            System.out.println("avanzo pasos "+cantidadDePasos);
+            bateria.setCarga(bateria.getCarga()-consumo);
+        } else {
+            System.out.println("sin bateria");
+            System.out.println("desea recargar s/n");
+            
+        }
+    }
+
+    public void Retroceder(int cantidadDePasos) {
+     int consumo=(cantidadDePasos/100)*10;
+        if (dormido=false) {
+      
+ 
+     if (bateria.getCarga()>=consumo) {
+            System.out.println("retrocediendo pasos "+cantidadDePasos);
+            bateria.setCarga(bateria.getCarga()-consumo);
+        } else {
+            System.out.println("sin bateria");
+            System.out.println("desea recargar s/n");
+    }
+    } else {
+            System.out.println("el robot esta dormido");
+        }
+    }
+    
+    public Robot() {
+       bateria=new Bateria();
+       dormido=false;
+       
+    
+    }
+
+    public void Dormir() {
+        dormido=true;
+        System.out.println("el robot esta durmiendo");
+    }
+
+    public void Despertar() {
+        dormido=false;
+        System.out.println("el robot esta despierto");
+    }
+
+    public void Recargar() {
+        bateria.setCarga(1000);
+      
+       
+    }
+
+    public boolean bateriaLLena() {
+        return bateria.getCarga()==1000;
+    }
+
+    public boolean bateriaVacia() {
+         return bateria.getCarga()==0;
+  
+    }
+    
+
+    public int energiaActual() {
+         return bateria.getCarga();
+  
+    }
+
+    @Override
+    public String toString() {
+        return "Robot{" + "bateria=" + bateria + ", dormido=" + dormido + '}';
+    }
+    
 }
