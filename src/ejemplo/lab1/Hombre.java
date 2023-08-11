ecb1128058ead51b1e97f6789f8e1c3b5b650e08
FiorellaAilen <fiorellaailen@gmail.com>
2023-08-11 18:18:16 -0300

    Correccion hombre

--- src/ejemplo/lab1/Hombre.java
+++ src/ejemplo/lab1/Hombre.java
@@ -16,6 +16,61 @@ public class Hombre {
         this.nombre = nombre;
         this.apellido = apellido;
     }
+
+    public Robot getJugarConRobot() {
+        return jugarConRobot;
+    }
+
+    public void setJugarConRobot(Robot jugarConRobot) {
+        this.jugarConRobot = jugarConRobot;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
     
+    public void JugarConRobot() {
+        System.out.println(" Hacer que el Robot Avance 500 pasos.");
+        jugarConRobot.Avanzar(500);
+        System.out.println("El robot Retroceda 20 pasos.");
+        jugarConRobot.Retroceder(20);
+        System.out.println("Informe por pantalla cuanta energÃ­a tiene el robot Actualmente.");
+        jugarConRobot.energiaActual();
+        System.out.println("Ponga el robot a dormir.");
+        jugarConRobot.Dormir();
+    }
+
+    @Override
+    public String toString() {
+        return "Hombre{" + "robot=" + jugarConRobot + '}';
+    }
     
 }
